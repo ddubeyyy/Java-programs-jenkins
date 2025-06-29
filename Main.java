0d07c6137cab262685d6ea50dbd921b504c7356a
Disha Dubey <disha.dubeyy22@gmail.com>
2025-06-30 05:20:50 +0530

    initial Commit

--- Main.java
+++ Main.java
@@ -4,5 +4,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("This is java program \n" + 
         "Output from java program: \n"+ new Date());
+        System.out.println("Hello");
     }
 }
